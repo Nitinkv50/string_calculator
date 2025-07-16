b6cf73ea5955d73d0b6552df5cecae79dc65a2d5
nitin <nitinkv50@gmail.com>
2025-07-16 15:54:38 +0530

    handled custom delimiter parsing of numbers

--- lib/string_adder.rb
+++ lib/string_adder.rb
@@ -2,6 +2,12 @@ class StringAdder
   def self.add(numbers)
     return 0 if numbers.empty?
 
+    if numbers.start_with?("//")
+      delimiter, numbers = numbers.split("\n", 2)
+      delimiter = delimiter[2..]
+      numbers = numbers.gsub(delimiter, ',')
+    end
+
     numbers.gsub("\n", ',').split(',').map(&:to_i).sum
   end
 end
--- spec/lib/string_adder_spec.rb
+++ spec/lib/string_adder_spec.rb
@@ -21,5 +21,9 @@ RSpec.describe StringAdder do
     it 'handles new lines between numbers' do
       expect(described_class.add("1\n2,3")).to eq(6)
     end
+
+    it 'supports custom delimiter defined in the format //;\n1;2' do
+      expect(described_class.add("//;\n1;2")).to eq(3)
+    end
   end
 end
