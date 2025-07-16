47a7d6466679b207d41ac2213c82decc85067e8e
nitin <nitinkv50@gmail.com>
2025-07-16 15:09:44 +0530

    Handle single number input in StringAdder

--- spec/lib/string_adder_spec.rb
+++ spec/lib/string_adder_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe StringAdder do
     it 'returns 0 for an empty string' do
       expect(described_class.add("")).to eq(0)
     end
+
+    it 'returns number for single digit string' do
+      expect(described_class.add("1")).to eq(1)
+    end
   end
 end
